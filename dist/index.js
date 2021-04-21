ed042de6c21c3dd61ae862557f1adcff55f7f3c8
Brandon Yanofsky <byanofsky@me.com>
2021-04-20 18:55:54 -0700

    Attempt to fix issue with Action not running

--- dist/index.js
+++ dist/index.js
@@ -4538,27 +4538,34 @@ const core = __nccwpck_require__(193);
 /**
  * TODO: Add JSDoc
  */
-async function main() {
-  let error = false;
+async function run() {
+  try {
+    core.info("Starting Write-Good action.");
+    let error = false;
 
-  // TODO: Allow input to override glob pattern.
-  const globber = await glob.create("**/*.md");
+    // TODO: Allow input to override glob pattern.
+    const globber = await glob.create("**/*.md");
 
-  for await (const file of globber.globGenerator()) {
-    const suggestions = writeGood(file);
-    if (suggestions.length > 0) {
-      core.error(`File: ${file}`);
-      core.error(JSON.stringify(suggestions));
-      error = true;
+    for await (const file of globber.globGenerator()) {
+      const suggestions = writeGood(file);
+      if (suggestions.length > 0) {
+        core.error(`File: ${file}`);
+        core.error(JSON.stringify(suggestions));
+        error = true;
+      }
     }
-  }
 
-  if (error) {
-    core.setFailed("One or more markdown files had an error.");
+    if (error) {
+      core.setFailed("One or more markdown files had an error.");
+    } else {
+      core.info("Completed successfully.");
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
 
-main();
+run();
 
 })();
 
