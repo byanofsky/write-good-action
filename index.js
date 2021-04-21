47dd3366a8a6fa9cc6453decc8cdc1d2acb913eb
Brandon Yanofsky <byanofsky@me.com>
2021-04-20 18:21:13 -0700

    Initial working example

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const glob = require("@actions/glob");
 const writeGood = require("write-good");
+const core = require("@actions/core");
 
-const suggestions = writeGood("So the cat was stolen.");
+/**
+ * TODO: Add JSDoc
+ */
+async function main() {
+  let error = false;
 
-console.log(suggestions);
+  // TODO: Allow input to override glob pattern.
+  const globber = await glob.create("**/*.md");
+
+  for await (const file of globber.globGenerator()) {
+    const suggestions = writeGood(file);
+    if (suggestions.length > 0) {
+      core.error(`File: ${file}`);
+      core.error(JSON.stringify(suggestions));
+      error = true;
+    }
+  }
+
+  if (error) {
+    core.setFailed("One or more markdown files had an error.");
+  }
+}
+
+main();
